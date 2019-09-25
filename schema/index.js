bfcf046a03989d8a10e8722adf75c14a0626d990
Justin Perry <just.perry@gmail.com>
2019-09-25 15:58:53 +0100

    Get person from SWAPI

--- schema/index.js
+++ schema/index.js
@@ -1,7 +1,22 @@
-const { GraphQLSchema, GraphQLObjectType } = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const PersonType = require("../types/Person");
+const axios = require("axios");
 
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType"
+  name: "RootQueryType",
+  fields: {
+    people: {
+      args: {
+        id: { type: GraphQLString }
+      },
+      type: PersonType,
+      resolve(parentValue, { id }) {
+        return axios
+          .get(`https://swapi.co/api/people/${id}`)
+          .then(response => response.data);
+      }
+    }
+  }
 });
 
 module.exports = new GraphQLSchema({
