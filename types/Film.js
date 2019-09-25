f6674ca4fc94e0b43b9c1656b63bf59b66e98a0a
Justin Perry <just.perry@gmail.com>
2019-09-25 16:57:41 +0100

    Add simple fields to FilmType

--- types/Film.js
+++ types/Film.js
@@ -5,6 +5,30 @@ module.exports = new GraphQLObjectType({
   fields: {
     title: {
       type: GraphQLString
+    },
+    director: {
+      type: GraphQLString
+    },
+    episodeID: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.episode_id;
+      }
+    },
+    openingCrawl: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.opening_crawl;
+      }
+    },
+    producer: {
+      type: GraphQLString
+    },
+    releaseDate: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.release_date;
+      }
     }
   }
 });
