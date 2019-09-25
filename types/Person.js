ba1635a08879fb25d063f9021fe5f704a3c3b4ab
Justin Perry <just.perry@gmail.com>
2019-09-25 16:19:32 +0100

    Add simple string fields to PersonType

--- types/Person.js
+++ types/Person.js
@@ -14,6 +14,48 @@ module.exports = new GraphQLObjectType({
       resolve(parentValue, args) {
         return parentValue.birth_year;
       }
+    },
+    eyeColor: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.eye_color;
+      }
+    },
+    gender: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.gender;
+      }
+    },
+    hairColor: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.hair_color;
+      }
+    },
+    height: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.height;
+      }
+    },
+    mass: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.mass;
+      }
+    },
+    skinColor: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.skin_color;
+      }
+    },
+    skinColor: {
+      type: GraphQLString,
+      resolve(parentValue, args) {
+        return parentValue.skin_color;
+      }
     }
   }
 });
