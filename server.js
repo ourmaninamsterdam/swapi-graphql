94166f7dd242994f69d33611a0eaaa6d10b3ce6c
Justin Perry <just.perry@gmail.com>
2019-09-30 12:08:26 +0100

    Few tweaks for server

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 
+const DEFAULT_PORT = 4000;
+const port = process.env.PORT || DEFAULT_PORT;
+const environment = process.env.NODE_ENV || "development";
+const isDevEnvironment = environment === "development";
+
 const app = express();
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true,
+    customFormatErrorFn: isDevEnvironment
+      ? error => ({
+          message: error.message,
+          locations: error.locations,
+          stack: error.stack ? error.stack.split("\n") : [],
+          path: error.path
+        })
+      : null
   })
 );
 
-app.listen(4000, () => {
-  console.log("SWAPI GraphQL listening on localhost:4000/graphql");
+app.listen(port, () => {
+  console.log(`SWAPI GraphQL listening on localhost:${port}/graphql`);
 });
